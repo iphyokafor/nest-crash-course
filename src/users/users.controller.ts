7162e3257e8e117e0a0c0e6f4fc05c67c7f486b1
Okafor Ifeoma Sandra <sandraifeoma22@gmail.com>
2021-11-10 14:19:05 +0100

    update swagger documentation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,48 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOkResponse({ type: User, isArray: true })
+  @ApiQuery({ name: 'name', required: false })
   @Get()
-  getUsers(): User[] {
-    return this.usersService.findAll();
-  }
-  @Get(':id')
-  getUserById(@Param('id') id: string): User {
-    return this.usersService.findById(Number(id));
+  getUsers(@Query('name') name: string): User[] {
+    return this.usersService.findAll(name);
   }
 
+  @ApiOkResponse({ type: User, description: 'fetch user by Id' })
+  @ApiNotFoundResponse()
+  @Get(':id')
+  getUserById(@Param('id') id: string): User {
+    const user = this.usersService.findById(Number(id));
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+
+  @ApiCreatedResponse({ type: User })
   @Post()
   createUser(@Body() body: CreateUserDto): User {
     return this.usersService.createUser(body);
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,9 +4,17 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  private users: User[] = [{ id: 0, name: 'Sandra' }];
+  private users: User[] = [
+    { id: 0, name: 'Sandra' },
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Melanie' },
+    { id: 3, name: 'Melanie' },
+  ];
 
-  findAll(): User[] {
+  findAll(name?: string): User[] {
+    if (name) {
+      return this.users.filter((user) => user.name === name);
+    }
     return this.users;
   }
 
