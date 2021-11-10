95596755e1b16638b5165b6c08053aeb2b1c2368
iphyokafor <sandraifeoma22@gmail.com>
2021-11-10 05:43:37 +0100

    initial commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [{ id: 0, name: 'Sandra' }];
+
+  findAll(): User[] {
+    return this.users;
+  }
+
+  findById(userId: number): User {
+    return this.users.find((user) => user.id === userId);
+  }
+
+  createUser(CreateUserDto: CreateUserDto): User {
+    const newUser = { id: Date.now(), ...CreateUserDto };
+
+    this.users.push(newUser);
+    return newUser;
+  }
+}
